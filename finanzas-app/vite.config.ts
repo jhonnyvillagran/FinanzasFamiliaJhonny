dd5b332e7ed94ae166db0e9ad87e03240f4d1bd6
jhonnyvillagran <55518405+jhonnyvillagran@users.noreply.github.com>
2025-12-04 00:17:42 -0300

    Delete finanzas-app directory

--- finanzas-app/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // IMPORTANTE: Esto permite que funcione en subcarpetas de GitHub Pages
-  // Eliminado 'define' manual para process.env para usar el sistema nativo de Vite (import.meta.env)
-});
